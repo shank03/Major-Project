f3bd6ffdac0b30b3d53710be5e30272440cbf8ad
Shashank Verma <shashank.verma2002@gmail.com>
2023-12-19 09:54:45 +0530

    tcp: Convert tcp_nfq and csm to class based
    Signed-off-by: Shashank Verma <shashank.verma2002@gmail.com>

--- tcp/src/csm.hpp
+++ tcp/src/csm.hpp
@@ -3,100 +3,125 @@
 #define MAX_CS_REQ_DROP       3    // num of packet drops to wait until considering timeout
 #define CONGESTION_STATE_PORT 16
 #define CONGESTION_STATE_TTL  (10 * 1000)    // 10s
+#define CSM_NH_IP             "255.255.255.254"
+#define CSM_TAG               "[CSM] "
 
 /* congestion state manager */
 namespace csm {
 
-    typedef struct congestion_state {
+    struct congestion_state {
         bool           send;     // all [true] / limited [false]
-        unsigned short value;    // 0/num of pkt
+        unsigned short value;    // 0 / num of pkt
         unsigned short delay;    // (limited) duration in micro
+    };
 
-        void update_state(congestion_state *state) {
-            send  = state->send;
-            value = state->value;
-            delay = state->delay;
+    class Manager {
+    private:
+        congestion_state *state;
+        uint64_t          state_ttl;
+        char             *csm_ip;
+
+        static uint64_t get_current_millis() {
+            auto time     = std::chrono::system_clock::now();
+            auto duration = time.time_since_epoch();
+            return std::chrono::duration_cast<std::chrono::milliseconds>(duration).count();
+        }
+
+        void renew_ttl() {
+            state_ttl = get_current_millis() + CONGESTION_STATE_TTL;
+        }
+
+    public:
+        explicit Manager(const char *nh_ip) {
+            state     = new congestion_state { true, 0, 0 };
+            state_ttl = 0;
+
+            csm_ip = new char[16] { 0 };
+            strcpy(csm_ip, nh_ip == nullptr ? CSM_NH_IP : nh_ip);
+        }
+
+        bool send() {
+            return state->send;
+        }
+
+        unsigned short value() {
+            return state->value;
+        }
+
+        unsigned short delay() {
+            return state->delay;
+        }
+
+        void update_state(congestion_state *st) {
+            state->send  = st->send;
+            state->value = st->value;
+            state->delay = st->delay;
+
+            renew_ttl();
+
+            std::cout << CSM_TAG << "state: s[" << (state->send ? "a" : "l") << "]; v[" << state->value << "]; d[" << state->delay << "];" << std::endl;
         };
-    } *CS;
 
-    static CS state     = new congestion_state { true, 0, 0 };
-    uint64_t  state_ttl = 0;
-
-    uint64_t get_current_millis() {
-        auto time     = std::chrono::system_clock::now();
-        auto duration = time.time_since_epoch();
-        return std::chrono::duration_cast<std::chrono::milliseconds>(duration).count();
-    }
-
-    bool state_ttl_expired() {
-        return state_ttl < get_current_millis();
-    }
-
-    void renew_ttl() {
-        state_ttl = get_current_millis() + CONGESTION_STATE_TTL;
-    }
-
-    int cs_req_udp(char *dest_ip) {
-        char server_ip[] = "255.255.255.254";
-        if (dest_ip != nullptr) {
-            strcpy(server_ip, dest_ip);
+        [[nodiscard]] bool has_state_ttl_expired() const {
+            return state_ttl < get_current_millis();
         }
 
-        int         udp_socket;
-        sockaddr_in server_addr {}, source_addr {};
-        char        buffer[1024];
-        int         ttl = 2;
+        int cs_req_state() {
+            int         udp_socket;
+            sockaddr_in server_addr {}, source_addr {};
+            char        buffer[1024];
+            int         ttl = 2;
 
-        // Create UDP socket
-        udp_socket = socket(AF_INET, SOCK_DGRAM, 0);
-        if (udp_socket < 0) {
-            std::cerr << "[CSM] Error creating socket" << std::endl;
-            return 1;
-        }
+            // Create UDP socket
+            udp_socket = socket(AF_INET, SOCK_DGRAM, 0);
+            if (udp_socket < 0) {
+                std::cerr << CSM_TAG << "Error creating socket" << std::endl;
+                return 1;
+            }
 
-        // Set source address and port
-        memset((char *) &source_addr, 0, sizeof(source_addr));
-        source_addr.sin_family      = AF_INET;
-        source_addr.sin_port        = htons(CONGESTION_STATE_PORT);
-        source_addr.sin_addr.s_addr = INADDR_ANY;
+            // Set source address and port
+            memset((char *) &source_addr, 0, sizeof(source_addr));
+            source_addr.sin_family      = AF_INET;
+            source_addr.sin_port        = htons(CONGESTION_STATE_PORT);
+            source_addr.sin_addr.s_addr = INADDR_ANY;
 
-        // Bind the socket to the source address
-        if (bind(udp_socket, (sockaddr *) &source_addr, sizeof(source_addr)) < 0) {
-            std::cerr << "[CSM] Error binding socket to the source address" << std::endl;
+            // Bind the socket to the source address
+            if (bind(udp_socket, (sockaddr *) &source_addr, sizeof(source_addr)) < 0) {
+                std::cerr << CSM_TAG << "Error binding socket to the source address" << std::endl;
+                close(udp_socket);
+                return 1;
+            }
+
+            // Set TTL value
+            if (setsockopt(udp_socket, IPPROTO_IP, IP_TTL, &ttl, sizeof(ttl)) < 0) {
+                std::cerr << CSM_TAG << "Error setting TTL" << std::endl;
+                close(udp_socket);
+                return 1;
+            }
+
+            // Server address and port
+            memset((char *) &server_addr, 0, sizeof(server_addr));
+            server_addr.sin_family      = AF_INET;
+            server_addr.sin_port        = htons(CONGESTION_STATE_PORT);
+            server_addr.sin_addr.s_addr = inet_addr(csm_ip);
+
+            // Message to send
+            const char *message = "Requested Congestion State";
+            strcpy(buffer, message);
+
+            // Send message
+            if (sendto(udp_socket, buffer, strlen(buffer), 0, (sockaddr *) &server_addr, sizeof(server_addr)) < 0) {
+                std::cerr << CSM_TAG << "Error sending message" << std::endl;
+                close(udp_socket);
+                return 1;
+            }
+
+            std::cout << CSM_TAG << "[" << csm_ip << "] Message sent" << std::endl;
+
+            // Close socket
             close(udp_socket);
-            return 1;
+
+            return 0;
         }
-
-        // Set TTL value
-        if (setsockopt(udp_socket, IPPROTO_IP, IP_TTL, &ttl, sizeof(ttl)) < 0) {
-            std::cerr << "[CSM] Error setting TTL" << std::endl;
-            close(udp_socket);
-            return 1;
-        }
-
-        // Server address and port
-        memset((char *) &server_addr, 0, sizeof(server_addr));
-        server_addr.sin_family      = AF_INET;
-        server_addr.sin_port        = htons(CONGESTION_STATE_PORT);
-        server_addr.sin_addr.s_addr = inet_addr(server_ip);
-
-        // Message to send
-        const char *message = "Requested Congestion State";
-        strcpy(buffer, message);
-
-        // Send message
-        if (sendto(udp_socket, buffer, strlen(buffer), 0, (sockaddr *) &server_addr, sizeof(server_addr)) < 0) {
-            std::cerr << "[CSM] Error sending message" << std::endl;
-            close(udp_socket);
-            return 1;
-        }
-
-        std::cout << "[CSM] [" << server_ip << "] Message sent" << std::endl;
-
-        // Close socket
-        close(udp_socket);
-
-        return 0;
-    }
-
+    };
 }    // namespace csm
--- tcp/src/tcp.hpp
+++ tcp/src/tcp.hpp
@@ -9,156 +9,188 @@
 #include "checksum.hpp"
 
 #define MAX_PAYLOAD_SIZE 4096
+#define NFQ_TAG          "[NFQ] "
 
 namespace tcp_nfq {
 
-    static std::atomic<bool>  udp_req_called      = false;
-    static std::atomic<short> packets_transferred = 0;
-    static std::atomic<short> packet_drops        = 0;
+    class Handler {
+    private:
+        Handler() = default;
 
-    static in_addr *source_ip   = nullptr;
-    static char    *next_hop_ip = nullptr;
+        static std::atomic<bool> looper;
 
-    static int packet_callback(nfq_q_handle *qh, nfgenmsg *nfmsg, nfq_data *nfad, void *data) {
-        nfqnl_msg_packet_hdr *ph;
-        uint32_t              packet_id;
-        unsigned char        *packet;
-        int                   packet_len;
+        static std::atomic<bool>  udp_req_called;
+        static std::atomic<short> packets_transferred;
+        static std::atomic<short> packet_drops;
 
-        ph         = nfq_get_msg_packet_hdr(nfad);
-        packet_id  = ntohl(ph->packet_id);
-        packet_len = nfq_get_payload(nfad, &packet);
+        static in_addr      *source_ip;
+        static in_addr_t     local_addr;
+        static csm::Manager *csm_manager;
 
-        // Manipulate the packet here if needed
-        // For example, modify TCP destination port
-        auto *ip_header = (iphdr *) packet;
-        if (ip_header->saddr == 16777343 || ip_header->daddr == 16777343) {
-            // Skip localhost
-            return nfq_set_verdict(qh, packet_id, NF_ACCEPT, 0, nullptr);
+        static void init(in_addr *src, char *nh_ip) {
+            looper              = true;
+            udp_req_called      = false;
+            packets_transferred = 0;
+            packet_drops        = 0;
+
+            source_ip   = src;
+            local_addr  = inet_addr("127.0.0.1");
+            csm_manager = new csm::Manager(nh_ip);
         }
 
-        if (ip_header->protocol == IPPROTO_UDP) {
-            if (ip_header->saddr == source_ip->s_addr) {
+        static int packet_callback(nfq_q_handle *qh, nfgenmsg *, nfq_data *nfad, void *) {
+            nfqnl_msg_packet_hdr *ph;
+            uint32_t              packet_id;
+            unsigned char        *packet;
+            int                   packet_len;
+
+            ph         = nfq_get_msg_packet_hdr(nfad);
+            packet_id  = ntohl(ph->packet_id);
+            packet_len = nfq_get_payload(nfad, &packet);
+
+            // Manipulate the packet here if needed
+            // For example, modify TCP destination port
+            auto *ip_header = (iphdr *) packet;
+            if (ip_header->saddr == local_addr || ip_header->daddr == local_addr) {
+                // Skip local_addr
                 return nfq_set_verdict(qh, packet_id, NF_ACCEPT, 0, nullptr);
             }
 
-            // Capture only those with immediate hop
-            if (ip_header->ttl - 1 != 0) {
-                return nfq_set_verdict(qh, packet_id, NF_ACCEPT, 0, nullptr);
-            }
-
-            auto *udp_header = (udphdr *) (packet + (ip_header->ihl << 2));
-            if (ntohs(udp_header->dest) != CONGESTION_STATE_PORT) {
-                return nfq_set_verdict(qh, packet_id, NF_ACCEPT, 0, nullptr);
-            }
-
-            std::cout << "[NFQ] [UDP] CS response\n";
-
-            std::cout << "--\n";
-            std::cout << "[NFQ] [UDP] source IP: " << inet_ntoa(in_addr { ip_header->saddr }) << std::endl;
-            std::cout << "source port: " << ntohs(udp_header->source) << std::endl;
-            std::cout << "dest port: " << ntohs(udp_header->dest) << std::endl;
-
-            const u_char *payload = packet + (ip_header->ihl << 2) + sizeof(udphdr);
-            //            int           payload_len = htons(udp_header->len) - sizeof(udphdr);
-
-            csm::state->update_state((csm::CS) payload);
-            csm::renew_ttl();
-
-            std::cout << "[NFQ] [CS] state: s: " << (csm::state->send ? "all" : "limited") << "; v: " << csm::state->value << "; d: " << csm::state->delay << std::endl;
-        }
-
-        if (ip_header->protocol == IPPROTO_TCP) {
-            if (ip_header->saddr != source_ip->s_addr) {
-                return nfq_set_verdict(qh, packet_id, NF_ACCEPT, 0, nullptr);
-            }
-
-            if (csm::state_ttl_expired()) {
-                if (packet_drops == MAX_CS_REQ_DROP) {
-                    packet_drops   = 0;
-                    udp_req_called = false;
+            if (ip_header->protocol == IPPROTO_UDP) {
+                if (ip_header->saddr == source_ip->s_addr) {
+                    return nfq_set_verdict(qh, packet_id, NF_ACCEPT, 0, nullptr);
                 }
 
-                if (udp_req_called) {
-                    packet_drops++;
-                    // Do not let any packet pass if we don't have congestion state
+                // Capture only those with immediate hop
+                if (ip_header->ttl - 1 != 0) {
+                    return nfq_set_verdict(qh, packet_id, NF_ACCEPT, 0, nullptr);
+                }
+
+                auto *udp_header = (udphdr *) (packet + (ip_header->ihl << 2));
+                if (ntohs(udp_header->dest) != CONGESTION_STATE_PORT) {
+                    return nfq_set_verdict(qh, packet_id, NF_ACCEPT, 0, nullptr);
+                }
+
+                std::cout << NFQ_TAG << "[UDP] CS response\n";
+
+                const u_char *payload = packet + (ip_header->ihl << 2) + sizeof(udphdr);
+                //            int           payload_len = htons(udp_header->len) - sizeof(udphdr);
+
+                csm_manager->update_state((csm::congestion_state *) payload);
+            }
+
+            if (ip_header->protocol == IPPROTO_TCP) {
+                if (ip_header->saddr != source_ip->s_addr) {
+                    return nfq_set_verdict(qh, packet_id, NF_ACCEPT, 0, nullptr);
+                }
+
+                if (csm_manager->has_state_ttl_expired()) {
+                    if (packet_drops == MAX_CS_REQ_DROP) {
+                        packet_drops   = 0;
+                        udp_req_called = false;
+                    }
+
+                    if (udp_req_called) {
+                        packet_drops++;
+                        // Do not let any packet pass if we don't have congestion state
+                        return nfq_set_verdict(qh, packet_id, NF_DROP, 0, nullptr);
+                    }
+
+                    std::cout << NFQ_TAG << "[CS] TTL expired. Renewing" << std::endl;
+                    udp_req_called = csm_manager->cs_req_state() == 0;
                     return nfq_set_verdict(qh, packet_id, NF_DROP, 0, nullptr);
                 }
+                udp_req_called = false;
+                packet_drops   = 0;
 
-                std::cout << "[NFQ] [CS] TTL expired. Renewing" << std::endl;
-                udp_req_called = csm::cs_req_udp(next_hop_ip) == 0;
-                return nfq_set_verdict(qh, packet_id, NF_DROP, 0, nullptr);
-            }
-            udp_req_called = false;
-            packet_drops   = 0;
+                auto *tcp_header = (tcphdr *) (packet + (ip_header->ihl << 2));
 
-            auto *tcp_header = (tcphdr *) (packet + (ip_header->ihl << 2));
+                if (!csm_manager->send()) {
+                    tcp_header->cwr    = 1;
+                    tcp_header->window = htons(csm_manager->value());
 
-            if (!csm::state->send) {
-                tcp_header->cwr    = 1;
-                tcp_header->window = htons(csm::state->value);
+                    ip_header->check  = update_ip_checksum(ip_header);
+                    tcp_header->check = update_tcp_checksum(ip_header, tcp_header);
 
-                ip_header->check  = update_ip_checksum(ip_header);
-                tcp_header->check = update_tcp_checksum(ip_header, tcp_header);
-
-                if (packets_transferred == csm::state->value) {
-                    if (csm::state->delay > 0) {
-                        std::cout << "[NFQ] [CS] Delaying for " << csm::state->delay << "us" << std::endl;
-                        std::this_thread::sleep_for(std::chrono::microseconds(csm::state->delay));
+                    if (packets_transferred == csm_manager->value()) {
+                        if (csm_manager->delay() > 0) {
+                            std::cout << NFQ_TAG << "[CS] Delaying for " << csm_manager->delay() << "us" << std::endl;
+                            std::this_thread::sleep_for(std::chrono::microseconds(csm_manager->delay()));
+                        }
+                        packets_transferred = 0;
                     }
-                    packets_transferred = 0;
+                    packets_transferred++;
+
+                    return nfq_set_verdict(qh, ntohl(ph->packet_id), NF_ACCEPT, packet_len, packet);
                 }
-                packets_transferred++;
-
-                return nfq_set_verdict(qh, ntohl(ph->packet_id), NF_ACCEPT, packet_len, packet);
             }
+
+            // Accept the modified packet
+            // return nfq_set_verdict(qh, ntohl(ph->packet_id), NF_ACCEPT, payload_len, payload);
+            return nfq_set_verdict(qh, packet_id, NF_ACCEPT, 0, nullptr);
+        };
+
+    public:
+        static void stop() {
+            looper = false;
+            csm_manager->cs_req_state();
         }
 
-        // Accept the modified packet
-        // return nfq_set_verdict(qh, ntohl(ph->packet_id), NF_ACCEPT, payload_len, payload);
-        return nfq_set_verdict(qh, packet_id, NF_ACCEPT, 0, nullptr);
-    }
+        static int start(in_addr *src, char *nh_ip) {
+            init(src, nh_ip);
 
-    int start_handle(in_addr *src, char *nh_ip) {
-        source_ip   = src;
-        next_hop_ip = nh_ip;
+            nfq_handle   *handle;
+            nfq_q_handle *queue_handle;
 
-        nfq_handle   *handle;
-        nfq_q_handle *queue_handle;
+            int  fd, rv;
+            char buf[MAX_PAYLOAD_SIZE];
 
-        int  fd, rv;
-        char buf[MAX_PAYLOAD_SIZE];
+            handle = nfq_open();
+            if (!handle) {
+                std::cerr << NFQ_TAG << "Error during nfq_open()" << std::endl;
+                return 1;
+            }
 
-        handle = nfq_open();
-        if (!handle) {
-            std::cerr << "Error during nfq_open()" << std::endl;
-            return 1;
+            if (nfq_unbind_pf(handle, AF_INET) < 0) {
+                std::cerr << NFQ_TAG << "Error during nfq_bind_pf()" << std::endl;
+                return 1;
+            }
+
+            queue_handle = nfq_create_queue(handle, 0, &packet_callback, nullptr);
+            if (!queue_handle) {
+                std::cerr << NFQ_TAG << "Error during nfq_create_queue()" << std::endl;
+                return 1;
+            }
+
+            if (nfq_set_mode(queue_handle, NFQNL_COPY_PACKET, 0xFFFF) < 0) {
+                std::cerr << NFQ_TAG << "Can't set packet_copy mode" << std::endl;
+                return 1;
+            }
+
+            std::cout << NFQ_TAG << "Starting handle" << std::endl;
+
+            fd = nfq_fd(handle);
+            while (looper && (rv = recv(fd, buf, sizeof(buf), 0)) && rv >= 0) {
+                nfq_handle_packet(handle, buf, rv);
+            }
+
+            nfq_destroy_queue(queue_handle);
+            nfq_close(handle);
+
+            std::cout << NFQ_TAG << "Stopped handle" << std::endl;
+
+            return 0;
         }
+    };
 
-        if (nfq_unbind_pf(handle, AF_INET) < 0) {
-            std::cerr << "Error during nfq_bind_pf()" << std::endl;
-            return 1;
-        }
+    std::atomic<bool> Handler::looper = true;
 
-        queue_handle = nfq_create_queue(handle, 0, &packet_callback, nullptr);
-        if (!queue_handle) {
-            std::cerr << "Error during nfq_create_queue()" << std::endl;
-            return 1;
-        }
+    std::atomic<bool>  Handler::udp_req_called      = false;
+    std::atomic<short> Handler::packets_transferred = 0;
+    std::atomic<short> Handler::packet_drops        = 0;
 
-        if (nfq_set_mode(queue_handle, NFQNL_COPY_PACKET, 0xFFFF) < 0) {
-            std::cerr << "Can't set packet_copy mode" << std::endl;
-            return 1;
-        }
+    in_addr      *Handler::source_ip   = nullptr;
+    in_addr_t     Handler::local_addr  = 0;
+    csm::Manager *Handler::csm_manager = nullptr;
 
-        fd = nfq_fd(handle);
-        while ((rv = recv(fd, buf, sizeof(buf), 0)) && rv >= 0) {
-            nfq_handle_packet(handle, buf, rv);
-        }
-
-        nfq_destroy_queue(queue_handle);
-        nfq_close(handle);
-
-        return 0;
-    }
 }    // namespace tcp_nfq
